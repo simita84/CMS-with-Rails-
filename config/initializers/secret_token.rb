a89062df1a0ed16d411c4586031542475f288442
Simi Tresa Antony <Joanna@Simi-Tresa-Antonys-MacBook-Pro-2.local>
2014-01-13 19:09:34 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomsAndTots::Application.config.secret_token = 'c5d53d18cdfcb335761fb9ecc177d9f033980c37ebc4be60b333c7da6ba0319a43f5a764e38bc52c1a700ef56b9fa6642aa0e2ee178c91f68598e81fc68fca90'

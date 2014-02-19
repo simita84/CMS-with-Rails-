873e6ef9a17fea1a7415b9d048d5c1d7d1f739dc
Simi Tresa Antony <simiraj2012@csu.fullerton.edu>
2014-02-18 23:22:04 -0800

    Fixed url for config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ MomsAndTots::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => "http://murmuring-garden-1315.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "http://murmuring-garden-1315.herokuapp.com" }
   
   
   config.action_mailer.delivery_method = :smtp

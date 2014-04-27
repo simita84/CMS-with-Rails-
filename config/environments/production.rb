2f72b4e2693a8c64a7c17175ca03a45c1b066968
Simi Tresa Antony <simiraj2012@csu.fullerton.edu>
2014-04-26 23:05:38 -0700

    Configuring paperclip for heroku using AWS-s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,11 @@ config.paperclip_defaults = {
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :path => ":class/:attachment/:id/:style/:filename",
+    :url => ':s3_domain_url'},
+  :s3_protocol    => "https",
+  :s3_host_name   => "s3-us-west-2.amazonaws.com"
   }
 }
 
